532542d6a05b0f634295c76961acce0fe0729440
lokeshn1996 <lokesh.github@gmail.com>
2020-11-27 15:33:03 +0530

    adding database code

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring5webappApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		//we use directly master branch for coding.
+		//it is the 1st project of own udemy course
 		SpringApplication.run(Spring5webappApplication.class, args);
 	}
 }
